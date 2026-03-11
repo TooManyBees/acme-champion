320d1cbb781404f4954785edc52808b0aec39b4c
Jess Bees <hi@toomanybees.com>
2026-03-11 03:20:11 -0400

    fake adding and removing challenges

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::pin::Pin;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use http_body_util::{BodyExt, Empty, Full, combinators::BoxBody};
 use hyper::{
@@ -14,7 +14,7 @@ use hyper_util::rt::TokioIo;
 use tokio::{
     net::{TcpListener, TcpStream},
     signal::ctrl_c,
-    sync::Mutex,
+    // sync::Mutex,
 };
 
 type Challenges = Arc<Mutex<HashMap<String, String>>>;
@@ -30,19 +30,53 @@ impl Service<Request<Incoming>> for Champion {
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn call(&self, req: Request<Incoming>) -> Self::Future {
+        let fake_domain = String::from("hithere.com");
+        let fake_name = String::from("_acme_challenge.hithere.com");
+        let fake_challenge = String::from("super-secret-string");
         let resp = match (req.method(), req.uri().path()) {
+            (&Method::GET, "/") => {
+                let result = self.get_challenges();
+                Ok(full_response(StatusCode::OK, result))
+            }
             (&Method::POST, "/register") => {
+                self.set_challenge(fake_domain, fake_name, fake_challenge);
                 Ok(empty_response(StatusCode::CREATED))
-            },
+            }
             (&Method::DELETE, "/register") => {
+                self.unset_challenge(fake_domain, fake_name);
                 Ok(empty_response(StatusCode::NO_CONTENT))
             }
-            _ => {
-                Ok(empty_response(StatusCode::NOT_FOUND))
-            }
+            _ => Ok(empty_response(StatusCode::NOT_FOUND)),
         };
 
         Box::pin(async { resp })
+    }
+}
+
+impl Champion {
+    fn get_challenges(&self) -> String {
+        let challenges = self.challenges.lock().unwrap();
+        let mut result = String::new();
+        for (key, value) in challenges.iter() {
+            result.push_str(key);
+            result.push(' ');
+            result.push_str(value);
+            result.push('\n');
+        }
+        result
+    }
+
+    fn set_challenge(&self, _name: String, txt_name: String, txt_value: String) {
+        let mut challenges = self.challenges.lock().unwrap();
+        if challenges.contains_key(&txt_name) {
+            eprintln!("Overwriting existing challenge for {txt_name}");
+        }
+        challenges.insert(txt_name, txt_value);
+    }
+
+    fn unset_challenge(&self, _name: String, txt_name: String) {
+        let mut challenges = self.challenges.lock().unwrap();
+        challenges.remove(&txt_name);
     }
 }
 
@@ -97,7 +131,15 @@ fn empty_body() -> BoxBody<Bytes, hyper::Error> {
         .map_err(|never| match never {})
         .boxed()
 }
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+fn full_response<T: Into<Bytes>>(
+    status_code: StatusCode,
+    chunk: T,
+) -> Response<BoxBody<Bytes, hyper::Error>> {
+    let mut resp = Response::new(full_body(chunk));
+    *resp.status_mut() = status_code;
+    resp
+}
+fn full_body<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
     Full::new(chunk.into())
         .map_err(|never| match never {})
         .boxed()
