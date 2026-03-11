fa133d89a2ca2e14a858de0c30f47e63f2cdf77a
Jess Bees <hi@toomanybees.com>
2026-03-11 02:28:10 -0400

    use a service with challenges in it

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use std::collections::HashMap;
-use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::pin::Pin;
 use std::sync::Arc;
 
-use http_body_util::Full;
+use http_body_util::{BodyExt, Empty, Full, combinators::BoxBody};
 use hyper::{
-    Request,
-    Response,
-    body::Bytes,
+    Method, Request, Response, StatusCode,
+    body::{Bytes, Incoming},
     server::conn::http1,
-    service::service_fn,
+    service::Service,
 };
 use hyper_util::rt::TokioIo;
 use tokio::{
@@ -20,6 +19,33 @@ use tokio::{
 
 type Challenges = Arc<Mutex<HashMap<String, String>>>;
 
+#[derive(Clone, Debug)]
+struct Champion {
+    challenges: Challenges,
+}
+
+impl Service<Request<Incoming>> for Champion {
+    type Response = Response<BoxBody<Bytes, hyper::Error>>;
+    type Error = hyper::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn call(&self, req: Request<Incoming>) -> Self::Future {
+        let resp = match (req.method(), req.uri().path()) {
+            (&Method::POST, "/register") => {
+                Ok(empty_response(StatusCode::CREATED))
+            },
+            (&Method::DELETE, "/register") => {
+                Ok(empty_response(StatusCode::NO_CONTENT))
+            }
+            _ => {
+                Ok(empty_response(StatusCode::NOT_FOUND))
+            }
+        };
+
+        Box::pin(async { resp })
+    }
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let http_addr = SocketAddr::from(([127, 0, 0, 1], 8053));
@@ -27,10 +53,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let challenges: Challenges = Arc::new(Mutex::new(HashMap::new()));
 
+    let champion = Champion {
+        challenges,
+    };
+
     loop {
         tokio::select! {
             Ok((stream, _)) = http_listener.accept() => {
-                handle_http(stream, challenges.clone());
+                let service = champion.clone();
+                handle_http(stream, service);
             }
             _ = shutdown_signal() => {
                 // TODO: unpack listen_http above so we can drop http_listener
@@ -47,18 +78,27 @@ async fn shutdown_signal() {
     ctrl_c().await.expect("failed to install signal handler");
 }
 
-fn handle_http(stream: TcpStream, _challenges: Challenges) {
+fn handle_http(stream: TcpStream, service: Champion) {
     let io = TokioIo::new(stream);
     tokio::task::spawn(async move {
-        if let Err(err) = http1::Builder::new()
-            .serve_connection(io, service_fn(responder))
-            .await
-        {
+        if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
             eprintln!("Error serving connection: {:?}", err);
         }
     });
 }
 
-async fn responder(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("response"))))
+fn empty_response(status_code: StatusCode) -> Response<BoxBody<Bytes, hyper::Error>> {
+    let mut resp = Response::new(empty_body());
+    *resp.status_mut() = status_code;
+    resp
+}
+fn empty_body() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
