cd9aa69ca778bc16fde18c3f5732cbb16f7273d5
Jess Bees <hi@toomanybees.com>
2026-03-27 23:43:14 -0400

    move response sending out of dns handle_request

--- src/dns_handler.rs
+++ src/dns_handler.rs
@@ -18,8 +18,8 @@ pub async fn bind_udp_stream(addr: SocketAddr) -> std::io::Result<UdpStream> {
     Ok(UdpStream::new(Arc::new(dns_socket_4)))
 }
 
-pub fn handle_dns(message: Vec<u8>, handler: Responder, challenges: &Arc<Challenges>) {
-    let src_addr = handler.addr();
+pub fn handle_dns(message: Vec<u8>, responder: Responder, challenges: &Arc<Challenges>) {
+    let src_addr = responder.addr();
     tracing::debug!(remote_addr = %src_addr, "new UDP message");
     if !valid_return_address(&src_addr) {
         tracing::warn!(addr = %src_addr, "ignoring DNS request with invalid return address");
@@ -29,22 +29,20 @@ pub fn handle_dns(message: Vec<u8>, handler: Responder, challenges: &Arc<Challen
     let challenges = challenges.clone();
     tokio::task::spawn(
         async move {
-            match handle_request(message, challenges, handler).await {
-                Err(e) => {
-                    tracing::error!(error = %e, "error handling DNS request");
+            if let Some(response) = handle_request(message, challenges).await {
+                match responder.send(response).await {
+                    Err(e) => {
+                        tracing::error!(error = %e, "error handling DNS request");
+                    }
+                    _ => {}
                 }
-                _ => {}
             }
         }
         .instrument(tracing::info_span!("process DNS query", remote_addr = %src_addr)),
     );
 }
 
-async fn handle_request(
-    message: Vec<u8>,
-    challenges: Arc<Challenges>,
-    responder: Responder,
-) -> std::io::Result<()> {
+async fn handle_request(message: Vec<u8>, challenges: Arc<Challenges>) -> Option<Vec<u8>> {
     let (mut response, query_name, query_type, challenge_key) = match response_for_message(&message)
     {
         ReadMessageResult::Process {
@@ -54,11 +52,9 @@ async fn handle_request(
             challenge_key,
         } => (response, query_name, query_type, challenge_key),
         ReadMessageResult::EarlyExit(response) => {
-            let response_bytes = response.to_bytes();
-            responder.send(response_bytes).await?;
-            return Ok(());
+            return Some(response.to_bytes());
         }
-        ReadMessageResult::DontRespond => return Ok(()),
+        ReadMessageResult::DontRespond => return None,
     };
 
     match query_type {
@@ -85,11 +81,7 @@ async fn handle_request(
     tracing::debug!(?response);
     tracing::info!(challenge_name = %challenge_key, rcode = ?response.rcode, "answered DNS query");
 
-    let response_bytes = response.to_bytes();
-
-    responder.send(response_bytes).await?;
-
-    Ok(())
+    Some(response.to_bytes())
 }
 
 fn valid_return_address(src_addr: &SocketAddr) -> bool {
