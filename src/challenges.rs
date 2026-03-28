ec3771a45ba86ac7a8b56ae65520ecce4513fadd
Jess Bees <hi@toomanybees.com>
2026-03-28 02:58:53 -0400

    use a rwlock instead of a mutex for challenges

--- src/challenges.rs
+++ src/challenges.rs
@@ -1,5 +1,5 @@
 use std::collections::LinkedList;
-use tokio::sync::{Mutex, MutexGuard};
+use tokio::sync::{RwLock, RwLockReadGuard};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Challenge {
@@ -9,19 +9,19 @@ pub struct Challenge {
 }
 
 #[derive(Debug)]
-pub struct Challenges(Mutex<LinkedList<Challenge>>);
+pub struct Challenges(RwLock<LinkedList<Challenge>>);
 
 impl Challenges {
     pub fn new() -> Challenges {
-        Challenges(Mutex::new(LinkedList::new()))
+        Challenges(RwLock::new(LinkedList::new()))
     }
 
-    pub async fn all(&self) -> MutexGuard<'_, LinkedList<Challenge>> {
-        self.0.lock().await
+    pub async fn all(&self) -> RwLockReadGuard<'_, LinkedList<Challenge>> {
+        self.0.read().await
     }
 
     pub async fn any(&self, name: &str) -> bool {
-        let cs = self.0.lock().await;
+        let cs = self.0.read().await;
         for c in cs.iter() {
             if c.name == name {
                 return true;
@@ -31,7 +31,7 @@ impl Challenges {
     }
 
     pub async fn named(&self, name: &str) -> Vec<String> {
-        let cs = self.0.lock().await;
+        let cs = self.0.read().await;
         let mut matching = Vec::with_capacity(1);
         for c in cs.iter() {
             if c.name == name {
@@ -42,12 +42,12 @@ impl Challenges {
     }
 
     pub async fn set(&self, challenge: Challenge) {
-        let mut cs = self.0.lock().await;
+        let mut cs = self.0.write().await;
         cs.push_back(challenge);
     }
 
     pub async fn cleanup(&self, challenge: &Challenge) {
-        let mut cs = self.0.lock().await;
+        let mut cs = self.0.write().await;
         cs.extract_if(|c| c == challenge).for_each(drop);
     }
 }
