59329bf749fa9e48b0b3a08271b29b297350d7f6
Jess Bees <hi@toomanybees.com>
2026-04-09 01:05:09 -0400

    cargo fmt, thanks

--- src/logger.rs
+++ src/logger.rs
@@ -19,9 +19,7 @@ pub fn init_tracing(level: Level, format: LogFormat) {
         }
         #[cfg(feature = "journald")]
         LogFormat::Journald => match tracing_journald::layer() {
-            Ok(journald_layer) => {
-                journald = Some(journald_layer)
-            }
+            Ok(journald_layer) => journald = Some(journald_layer),
             Err(e) => {
                 eprintln!("Could not initialize journald: {e}");
                 plain = Some(tracing_subscriber::fmt::layer().compact().with_ansi(false));
